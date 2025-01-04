855e81068b31e5c27ae3374b18a77e5227e3a4a3
Raza749 <razaali.webdev@gmail.com>
2025-01-04 21:17:46 +0500

    solve the 3rd problem

--- 03_operators/challenge.cpp
+++ 03_operators/challenge.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
 
-    // Challenge 1: Arithmetic Operators
+    // Challenge 1: Arithmetic Operators*********
     // int teaPacks;
     // float pricePerPack, totalPrice;
 
@@ -17,18 +17,36 @@ int main()
     // totalPrice += totalPrice * 0.10;
     // cout << "Your total price is : " << totalPrice;
 
-    // Challenge 2: Assignment Operators
-    int teaBags;
+    // Challenge 2: Assignment Operators*********
+    // int teaBags;
 
-    cout << "Enter the number of tea bags you have :" << endl;
-    cin >> teaBags;
+    // cout << "Enter the number of tea bags you have :" << endl;
+    // cin >> teaBags;
 
-    if (teaBags < 20)
+    // if (teaBags < 20)
+    // {
+    //     teaBags += 10;
+    // }
+
+    // cout << "You have a total tea of tea bags: " << teaBags << endl;
+
+    // Challenge 3: Relational and Logical Operators*****
+    int cups;
+    bool isMemberForOneYear ;
+
+    cout << "Enter the cups you have : " << endl;
+    cin >> cups;
+    cout << "Are you a member for more than one year? (Enter 1 for Yes, 0 for No):" << endl;
+    cin >> isMemberForOneYear ;
+
+    if (isMemberForOneYear  || cups > 12)
     {
-        teaBags += 10;
+        cout << "You qualify for the discount offer!" << endl;
     }
-
-    cout << "You have a total tea of tea bags: " << teaBags << endl;
+    else
+    {
+        cout << "You do not qualify for the discount offer." << endl;
+    }
 
     return 0;
 };
