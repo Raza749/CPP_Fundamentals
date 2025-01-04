755bc5b9257844afda875f5d5d388f1e14025f7e
Raza749 <razaali.webdev@gmail.com>
2025-01-04 21:04:18 +0500

    solve the 2nd problem

--- 03_operators/challenge.cpp
+++ 03_operators/challenge.cpp
@@ -6,23 +6,29 @@ int main()
 {
 
     // Challenge 1: Arithmetic Operators
-    int teaPacks;
-    float pricePerPack, totalPrice;
+    // int teaPacks;
+    // float pricePerPack, totalPrice;
 
-    cout << "Enter the tea packs would you like to have:" << endl;
-    cin >> teaPacks;
-    cout << "Enter the price per pack:" << endl;
-    cin >> pricePerPack;
-    totalPrice = teaPacks * pricePerPack;
-    totalPrice += totalPrice * 0.10;
-    cout << "Your total price is : " << totalPrice;
+    // cout << "Enter the tea packs would you like to have:" << endl;
+    // cin >> teaPacks;
+    // cout << "Enter the price per pack:" << endl;
+    // cin >> pricePerPack;
+    // totalPrice = teaPacks * pricePerPack;
+    // totalPrice += totalPrice * 0.10;
+    // cout << "Your total price is : " << totalPrice;
 
     // Challenge 2: Assignment Operators
+    int teaBags;
 
+    cout << "Enter the number of tea bags you have :" << endl;
+    cin >> teaBags;
 
+    if (teaBags < 20)
+    {
+        teaBags += 10;
+    }
 
-
-
+    cout << "You have a total tea of tea bags: " << teaBags << endl;
 
     return 0;
 };
