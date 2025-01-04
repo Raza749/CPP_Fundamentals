090e8e34698a958e7dd77ab18bc4a8abfd3d1c3c
Raza749 <razaali.webdev@gmail.com>
2025-01-04 21:24:22 +0500

    push the problem no 4

--- 03_operators/challenge.cpp
+++ 03_operators/challenge.cpp
@@ -31,22 +31,53 @@ int main()
     // cout << "You have a total tea of tea bags: " << teaBags << endl;
 
     // Challenge 3: Relational and Logical Operators*****
-    int cups;
-    bool isMemberForOneYear ;
+    // int cups;
+    // bool isMemberForOneYear ;
 
-    cout << "Enter the cups you have : " << endl;
-    cin >> cups;
-    cout << "Are you a member for more than one year? (Enter 1 for Yes, 0 for No):" << endl;
-    cin >> isMemberForOneYear ;
+    // cout << "Enter the cups you have : " << endl;
+    // cin >> cups;
+    // cout << "Are you a member for more than one year? (Enter 1 for Yes, 0 for No):" << endl;
+    // cin >> isMemberForOneYear ;
 
-    if (isMemberForOneYear  || cups > 12)
+    // if (isMemberForOneYear  || cups > 12)
+    // {
+    //     cout << "You qualify for the discount offer!" << endl;
+    // }
+    // else
+    // {
+    //     cout << "You do not qualify for the discount offer." << endl;
+    // }
+
+    // Challenge 4: Bitwise Operators******
+    // Stock encoding: 1 = Green, 2 = Black, 4 = Oolong
+    int stock = 7; // Example stock: All teas are in stock (1 | 2 | 4 = 7)
+    int teaType;
+
+    // Display menu for tea types
+    cout << "Tea Types:\n";
+    cout << "1. Green Tea\n";
+    cout << "2. Black Tea\n";
+    cout << "4. Oolong Tea\n";
+    cout << "Enter the tea type you want to check (1, 2, or 4): ";
+    cin >> teaType;
+
+    // Check if the entered tea type is valid
+    if (teaType != 1 && teaType != 2 && teaType != 4)
     {
-        cout << "You qualify for the discount offer!" << endl;
+        cout << "Invalid tea type! Please enter 1, 2, or 4." << endl;
     }
     else
     {
-        cout << "You do not qualify for the discount offer." << endl;
-    }
+        // Use bitwise AND to check if the tea type is in stock
+        if (stock & teaType)
+        {
+            cout << "The selected tea type is in stock!" << endl;
+        }
+        else
+        {
+            cout << "The selected tea type is NOT in stock!" << endl;
+        }
 
-    return 0;
+        return 0;
+    }
 };
