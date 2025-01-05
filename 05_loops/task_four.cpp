a87b5c18bf04319423ace04ce99a994201c079cd
Raza749 <razaali.webdev@gmail.com>
2025-01-05 20:24:44 +0500

    learn about the break keyword and complete the task

---
+++ 05_loops/task_four.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string response;
+
+    while (true)
+    {
+        cout << "Do you want more tea (type 'stop' to exit)?" << endl;
+        getline(cin, response);
+
+        if (response == "stop")
+        {
+            // exit the loop
+            break;
+        }
+        cout << "Here is your another cup of tea." << endl;
+    }
+    cout << "No more tea will be served to you" << endl;
+
+    return 0;
+};
