18994edea64b35028f48393e94f6f3c5e25cb836
Raza749 <razaali.webdev@gmail.com>
2025-01-06 22:40:04 +0500

    write another function

--- 08_array/three.cpp
+++ 08_array/three.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
 using namespace std;
 
-int getScores(int scores[], int size)
+// int getScores(int scores[], int size)
+// {
+//     int total = 0;
+//     for (int i = 0; i < size; i++)
+//     {
+//         total += scores[i];
+//     }
+//     return total;
+// }
+
+double sum(double productPrices[], int numProducts)
 {
-    int total = 0;
-    for (int i = 0; i < size; i++)
+    double totalPrice = 0;
+    for (int i = 0; i < numProducts; i++)
     {
-        total += scores[i];
+        totalPrice += productPrices[i];
     }
-    return total;
-}
+    return totalPrice;
+};
 
 int main()
 {
-    // Array of scores
-    int scores[7] = {45, 54, 90, 23, 90, 45, 87};
+    // int scores[7] = {45, 54, 90, 23, 90, 45, 87};
 
-    // Calculate the total of scores
-    int totalScore = getScores(scores, 7);
+    // int totalScore = getScores(scores, 7);
 
-    // Display the total
-    cout << "Total Score: " << totalScore << endl;
+    // cout << "Total Score: " << totalScore << endl;
+
+    double productPrices[4] = {299.99, 49.99, 90.59, 90.34};
+
+    double totalPrice = sum(productPrices, 4);
+
+    cout << "The total price is : $" << totalPrice << endl;
 
     return 0;
 }
