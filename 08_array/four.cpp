4d6bf902815b4e327e4ad109074e44569a991410
Raza749 <razaali.webdev@gmail.com>
2025-01-06 23:18:10 +0500

    add four.cpp and learn something about pointers

--- 08_array/four.cpp
+++ 08_array/four.cpp
@@ -2,11 +2,28 @@
 
 using namespace std;
 
+int *productOrders(int order)
+{
+    int *orders = new int[order];
+    for (int i = 0; i < order; i++)
+    {
+        orders[i] = (i + 1) * 1000;
+    }
+    return orders;
+};
 
 int main()
 {
 
-   
+    int order = 5;
+    int *productOrder = productOrders(order);
+
+    for (int i = 0; i < order; i++)
+    {
+        cout << "Product Order " << i + 1 << ": " << productOrder[i] << endl;
+    }
+
+    delete[] productOrder;
 
     return 0;
 }
