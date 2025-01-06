5f4382bdd2d6637ceed8d8ed9bb7362f8aa389f7
Raza749 <razaali.webdev@gmail.com>
2025-01-06 21:54:26 +0500

    add two.cpp

---
+++ 08_array/two.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    string names[5] = {
+        "Raza",
+        "Fari",
+        "Hassan",
+        "Hasnain",
+        "Haris"};
+
+    cout << "Name : " << names[4] << endl;
+
+    return 0;
+}
